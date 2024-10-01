dab7efa5577603c704687ae578303ce9458f67a4
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-10-01 16:00:35 +0200

    task list rendering

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+
+type Task = {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+
+}
 @Component({
   selector: 'app-task',
   standalone: true,
@@ -8,5 +17,5 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-
+  @Input({required:true}) task!: Task;
 }
