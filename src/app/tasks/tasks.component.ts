0d28692e60357a153b751a3649530fc602f26eac
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-10-01 23:47:24 +0200

    feature: unshifts info array of task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
 @Component({
   selector: 'app-tasks',
   standalone: true,
@@ -9,8 +10,8 @@ import { NewTaskComponent } from './new-task/new-task.component';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input({ required: true }) userId?: string;
-  @Input({ required: true }) name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
   isAddingTask:boolean = false; 
 
   tasks = [{
@@ -51,6 +52,17 @@ export class TasksComponent {
   }
 
   onCancelAddTask(){
+    this.isAddingTask = false; 
+  }
+
+  onAddTask(taskData:NewTaskData){
+    this.tasks.unshift({
+      id: new Date().getTime().toString(), 
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    });
     this.isAddingTask = false; 
   }
 }
